20015728524b27621453afe54754945917c41f11
kierandidi <kieran.didi@gmail.com>
2022-08-15 13:42:02 +1000

    Added application name to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,15 @@ output "application_id" {
   description = "Id of the EMR Serverless application, can be used to submit/view jobs from AWS CLI"
 }
 
+output "application_name" {
+  value       = aws_iam_role.EMRServerlessS3RuntimeRole.arn
+  description = "Name of the EMR Serverless application, can be used to submit/view jobs from AWS Console"
+}
+
 output "execution_role_arn" {
   value       = aws_iam_role.EMRServerlessS3RuntimeRole.arn
   description = "ARN of execution role, can be used to attach role for other purposes"
 }
+
+
+
