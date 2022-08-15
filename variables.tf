08b140707895e6d3d8943510b436faa74e09c107
kierandidi <kieran.didi@gmail.com>
2022-08-15 13:03:17 +1000

    Added README

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "application_name" {
     description = "EMR serverless application name"
-    default = "emr-serverless-application"
+    default = "application-emr-serverless"
 }
 variable "bucket_name" { 
     description = "The bucket in which scripts for the application will be stored"
-    default = "dp-emr-serverless"
+    default = "bucket-emr-serverless"
 }
 variable "application_max_memory" {
     description = "The maximum memory available for the entire application."
-    default = "64 GB"
+    default = "4 GB"
 }
 variable "application_max_cores" {
     description = "The maximum CPU cores for the entire application."
-    default = "16 vCPU"
+    default = "1 vCPU"
 }
