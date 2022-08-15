fe096aec1a407723bd5707a72c2dcfa3697168e7
kierandidi <kieran.didi@gmail.com>
2022-08-15 13:22:16 +1000

    Added types for variables

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "application_name" {
+  type        = string
   description = "EMR serverless application name"
   default     = "application-emr-serverless"
 }
 variable "bucket_name" {
+  type        = string
   description = "The bucket in which scripts for the application will be stored"
   default     = "bucket-emr-serverless"
 }
 variable "application_max_memory" {
+  type        = string
   description = "The maximum memory available for the entire application."
   default     = "4 GB"
 }
 variable "application_max_cores" {
+  type        = string
   description = "The maximum CPU cores for the entire application."
   default     = "1 vCPU"
 }
